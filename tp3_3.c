bb77b361e36a77dbe029c6818b0d924be91e7d85
lucas <lucasvelasquez2001@gmail.com>
2023-04-17 03:51:42 -0300

    Agregando tp3_3.c modificado

--- tp3_3.c
+++ tp3_3.c
@@ -2,33 +2,44 @@
 #include <string.h>
 #include <stdlib.h>
 
-const int N = 5;
-
 int main()
 {
-
+    int cant;
     char **vector;
     char *Buff;
     
-    vector = (char **) malloc(sizeof(char) * N);
-    Buff = (char *) malloc(sizeof(char)*100);
+    printf("Ingrese la cantidad de nombres: ");
+    scanf("%d",&cant);
 
-    for (int i = 0; i < N; i++)
+    vector = (char **) malloc(sizeof(char) * cant);
+    
+    
+    for (int i = 0; i < cant; i++)
     {
+        Buff = (char *) malloc(sizeof(char)*100);
         printf("Ingrese un nombre: ");
+        fflush(stdin);
         gets(Buff);
 
-        vector[i] = (char *) malloc((strlen(Buff)+1) *sizeof(char));
+        vector[i] = (char *) malloc(sizeof(char) * (strlen(Buff)+1));
         strcpy(vector[i],Buff);
-        
+
+        free(Buff);
+    }
+
+    //mostrar
+    for (int i = 0; i < cant; i++)
+    {
         puts(vector[i]);
+
     }
     
     //tengo problemas con el ultimo nombre
     //preguntar porque es necesario utilizar el auxiliar
-    //falta liberar memoria
+    //falta liberar memoria de vector[i]
 
 
 
     return 0;
 }
+
